afb95c46e7322a90b97578a64096110e85c079b2
glex85 <glex85@gmail.com>
2023-02-17 17:11:15 +0900

    extract method

--- Ex3_VideoRental/Rental.java
+++ Ex3_VideoRental/Rental.java
@@ -1,49 +1,53 @@
 import java.util.Date;
 
 public class Rental {
-	private Video video ;
-	private int status ; // 0 for Rented, 1 for Returned (*Comments)
-	private Date rentDate ;
-	private Date returnDate ; // kj85.kim: temporary field
+    private Video video;
+    private int status; // 0 for Rented, 1 for Returned (*Comments)
+    private Date rentDate;
+    private Date returnDate; // kj85.kim: temporary field
 
-	public Rental(Video video) {
-		this.video = video ;
-		status = 0 ;
-		rentDate = new Date() ;
-	}
+    public Rental(Video video) {
+        this.video = video;
+        status = 0;
+        rentDate = new Date();
+    }
 
-	public Video getVideo() {
-		return video;
-	}
+    public Video getVideo() {
+        return video;
+    }
 
-	public int getStatus() {
-		return status;
-	}
+    public int getStatus() {
+        return status;
+    }
 
-	public void returnVideo() {
-		if ( status == 1 ) { // kj85.kim: temporary field
-			this.status = 1;
-			returnDate = new Date() ;
-		}
-	}
+    public void returnVideo() {
+        if (status == 1) { // kj85.kim: temporary field
+            this.status = 1;
+            returnDate = new Date();
+        }
+    }
 
-	public int getDaysRentedLimit() {
-		int limit = 0 ;
-		if ( getDaysRented() <= 2) return limit ;
-		return video.getVideoType().getLimit();
-	}
+    public int getDaysRentedLimit() {
+        int limit = 0;
+        if (getDaysRented() <= 2) return limit;
+        return video.getVideoType().getLimit();
+    }
 
-	public int getDaysRented() {
-		int daysRented;
-		if (getStatus() == 1) { // returned Video (*Comments)
-			long diff = returnDate.getTime() - rentDate.getTime();
-			//magic number
-			daysRented = (int) (diff / (1000 * 60 * 60 * 24)) + 1; //• Duplicate Code
-		} else { // not yet returned (*Comments)
-			long diff = new Date().getTime() - rentDate.getTime();
-			//magic number
-			daysRented = (int) (diff / (1000 * 60 * 60 * 24)) + 1; //• Duplicate Code
-		}
-		return daysRented;
-	}
+    public int getDaysRented() {
+        int daysRented;
+        if (getStatus() == 1) { // returned Video (*Comments)
+            daysRented = calcDaysRented(returnDate.getTime());
+        } else { // not yet returned (*Comments)
+            daysRented = calcDaysRented(new Date().getTime());
+        }
+        return daysRented;
+    }
+
+    private int calcDaysRented(long returnTime) {
+        int daysRented;
+        long diff = returnTime - rentDate.getTime();
+        //magic number
+        daysRented = (int) (diff / (1000 * 60 * 60 * 24)) + 1; //• Duplicate Code
+        return daysRented;
+    }
 }
